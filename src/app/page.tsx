6e039216670d129546614648d99f65a8699f10ac
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-08-07 08:42:05 +0530

    file upload button added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,15 @@
 
 import StatisticCard from "@/components/home-cards"
 import Home_herocard from "@/components/home-herocard"
+import { FileTriggerButton } from "@/components/ui/file-trigger";
+
 
 export default function Home(){
   return (
-    <div>
-    
+    <div className="p-10">
       <Home_herocard/>
       <StatisticCard />
+      <FileTriggerButton/>
     </div>
   )
 }
