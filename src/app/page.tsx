54b0b8a1492797b97d5a7c4656acbde1d414ac2a
jithendra259 <jithendrasubramanyam218@gmail.com>
2025-08-07 23:39:16 +0530

    bar chart and line chart is added

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,8 @@
 import StatisticCard from "@/components/home-cards"
 import Home_herocard from "@/components/home-herocard"
 import { FileTriggerButton } from "@/components/ui/file-trigger";
-
+import Linechartdata from "@/components/linechart"
+import MixedBarChartdata from "@/components/barchart";
 
 export default function Home(){
   return (
@@ -10,6 +11,8 @@ export default function Home(){
       <Home_herocard/>
       <StatisticCard />
       <FileTriggerButton/>
+      <Linechartdata/>
+      <MixedBarChartdata/>
     </div>
   )
 }
